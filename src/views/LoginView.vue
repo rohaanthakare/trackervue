89360f045a3b0a1537f48e7a19d030bef4f8b219
Rohan Thakare <rohaanthakare@gmail.com>
2023-04-06 19:39:01 +0530

    PrimeVue added and login page update

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,25 @@
 <script setup lang="ts">
+import Card from 'primevue/card';
 </script>
 
 <template>
-  <div>Login</div>
+  <div class="grid m-0 justify-content-center t-outer-form">
+    <div class="col-12 md:col-8 lg:col-6 xl:col-4">
+      <Card class="shadow-8">
+        <template #title>
+          <div class="flex justify-content-center pt-2 pb-2">
+            <img src="../assets/logos/tracker-light.png" style="width: 250px" />
+          </div>
+        </template>
+        <template #content>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam
+            deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate
+            neque
+            quas!
+          </p>
+        </template>
+      </Card>
+    </div>
+  </div>
 </template>
